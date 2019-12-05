82dfd1464f32d186e0945bfcf7e2591a7e60ae89
Menci <huanghaorui301@gmail.com>
2019-12-05 12:34:33 +0800

    feat(user): add user-privilege entity and service

---
+++ src/user/user-privilege.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Index,
+  ManyToOne,
+  Column,
+  JoinColumn
+} from "typeorm";
+
+import { UserEntity } from "@/user/user.entity";
+
+export enum UserPrivilegeType {
+  MANAGE_USER,
+  MANAGE_PROBLEM,
+  MANAGE_CONTEST,
+  MANAGE_DISCUSSION
+};
+
+@Entity("user-privilege")
+@Index(["userId", "privilegeType"], { unique: true })
+export class UserPrivilegeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(type => UserEntity, {
+    onDelete: "CASCADE"
+  })
+  @JoinColumn()
+  user: Promise<UserEntity>;
+
+  @Column()
+  @Index()
+  userId: number;
+
+  @Column({
+    type: "enum",
+    enum: UserPrivilegeType
+  })
+  @Index()
+  privilegeType: UserPrivilegeType
+}
