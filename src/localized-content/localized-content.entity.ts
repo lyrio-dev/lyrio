90b1bf1dc5f3f532ce7882396dde293bd7790912
Menci <huanghaorui301@gmail.com>
2019-12-08 02:12:46 +0800

    [localized-content] Add LocalizedContent module

---
+++ src/localized-content/localized-content.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryColumn, Column, Index } from "typeorm";
+import { Locale } from "@/common/locale.type";
+
+export enum LocalizedContentType {
+  PROBLEM_TITLE = "PROBLEM_TITLE",
+  PROBLEM_CONTENT = "PROBLEM_CONTENT"
+}
+
+@Entity("localized_content")
+@Index(["objectId", "type"])
+export class LocalizedContentEntity {
+  @PrimaryColumn({ type: "integer" })
+  objectId: number;
+
+  @PrimaryColumn({ type: "enum", enum: LocalizedContentType })
+  type: LocalizedContentType;
+
+  @PrimaryColumn({ type: "enum", enum: Locale })
+  locale: Locale;
+
+  @Column({ type: "mediumtext" })
+  data: string;
+}
