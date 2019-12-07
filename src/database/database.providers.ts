05a59a24b157c6340dfd215f171fa6db037abad3
Menci <huanghaorui301@gmail.com>
2019-12-08 02:11:16 +0800

    [database] Patch TypeORM to workaround typeorm/typeorm#3636

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,19 +3,40 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@/config/config.module";
 import { ConfigService } from "@/config/config.service";
 
+let patched = false;
+
+function patchTypeOrm() {
+  const TypeORMMysqlDriver = require("typeorm/driver/mysql/MysqlDriver");
+  const OriginalNormalizeType =
+    TypeORMMysqlDriver.MysqlDriver.prototype.normalizeType;
+  TypeORMMysqlDriver.MysqlDriver.prototype.normalizeType = function(column) {
+    if (column.type === "json") {
+      return "longtext";
+    }
+    return OriginalNormalizeType(column);
+  };
+}
+
 export const databaseProviders = [
   TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
-    useFactory: (configService: ConfigService) => ({
-      type: configService.config.database.type,
-      host: configService.config.database.host,
-      port: configService.config.database.port,
-      username: configService.config.database.username,
-      password: configService.config.database.password,
-      database: configService.config.database.database,
-      entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-      synchronize: true
-    }),
+    useFactory: (configService: ConfigService) => {
+      if (configService.config.database.type === "mariadb" && !patched) {
+        patchTypeOrm();
+        patched = true;
+      }
+
+      return {
+        type: configService.config.database.type,
+        host: configService.config.database.host,
+        port: configService.config.database.port,
+        username: configService.config.database.username,
+        password: configService.config.database.password,
+        database: configService.config.database.database,
+        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+        synchronize: true
+      };
+    },
     inject: [ConfigService]
   })
 ];
