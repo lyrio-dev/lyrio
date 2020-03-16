67105f1b1b41cfba8c53c2efc2f7efccabedc154
Menci <huanghaorui301@gmail.com>
2020-03-16 01:59:10 +0000

    feat(*): upgrade dependencies

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,40 +3,20 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@/config/config.module";
 import { ConfigService } from "@/config/config.service";
 
-let patched = false;
-
-function patchTypeOrm() {
-  const TypeORMMysqlDriver = require("typeorm/driver/mysql/MysqlDriver");
-  const OriginalNormalizeType = TypeORMMysqlDriver.MysqlDriver.prototype.normalizeType;
-  TypeORMMysqlDriver.MysqlDriver.prototype.normalizeType = function(column) {
-    if (column.type === "json") {
-      return "longtext";
-    }
-    return OriginalNormalizeType(column);
-  };
-}
-
 export const databaseProviders = [
   TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
-    useFactory: (configService: ConfigService) => {
-      if (configService.config.services.database.type === "mariadb" && !patched) {
-        patchTypeOrm();
-        patched = true;
-      }
-
-      return {
-        type: configService.config.services.database.type,
-        host: configService.config.services.database.host,
-        port: configService.config.services.database.port,
-        username: configService.config.services.database.username,
-        password: configService.config.services.database.password,
-        database: configService.config.services.database.database,
-        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-        logging: !!process.env["SYZOJ_NG_LOG_SQL"],
-        synchronize: true
-      };
-    },
+    useFactory: (configService: ConfigService) => ({
+      type: configService.config.services.database.type,
+      host: configService.config.services.database.host,
+      port: configService.config.services.database.port,
+      username: configService.config.services.database.username,
+      password: configService.config.services.database.password,
+      database: configService.config.services.database.database,
+      entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+      logging: !!process.env["SYZOJ_NG_LOG_SQL"],
+      synchronize: true
+    }),
     inject: [ConfigService]
   })
 ];
