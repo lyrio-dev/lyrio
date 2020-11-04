c3cec49eaf61bfed152d0b500db6ef85ca3b4a18
Menci <huanghaorui301@gmail.com>
2020-11-05 04:03:48 +0800

    feat(migration): replace confusing compilation error message of old submissions

--- src/migration/migrations/submission.ts
+++ src/migration/migrations/submission.ts
@@ -348,7 +348,16 @@ export const migrationSubmission: MigrationInterface = {
             result.status = submission.status;
 
             if (oldResult.compile) {
-              const oldCompileMessage = oldResult.compile.message || "";
+              let oldCompileMessage = oldResult.compile.message || "";
+
+              // Replace confusing error message
+              const matchResult = /Your source code compiled to (\d+) bytes which is too big, too thick, too long for us\.\./.exec(
+                oldCompileMessage
+              );
+              if (matchResult) {
+                oldCompileMessage = `The source code compiled to ${matchResult[1]} bytes, exceeding the size limit.`;
+              }
+
               const notConvertedRegex = /^A [a-zA-Z]+ encountered while compiling your code./;
               const notConverted = notConvertedRegex.test(oldCompileMessage);
               result.compile = {
