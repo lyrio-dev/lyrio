cabf750403c6fd0dd841c4e0e7a1a216ae9b683e
Menci <huanghaorui301@gmail.com>
2020-11-17 03:29:10 +0800

    fix(problem): fix change problem type API bad request

--- src/problem/dto/change-problem-type-request.dto.ts
+++ src/problem/dto/change-problem-type-request.dto.ts
@@ -1,11 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
 
+import { IsEnum, IsInt } from "class-validator";
+
 import { ProblemType } from "../problem.entity";
 
 export class ChangeProblemTypeRequestDto {
   @ApiProperty()
+  @IsInt()
   problemId: number;
 
   @ApiProperty()
+  @IsEnum(ProblemType)
   type: ProblemType;
 }
