29aa5d4cb7ff40be6bb17c4a3a3acbfe15d10ca1
Menci <huanghaorui301@gmail.com>
2020-11-19 15:39:06 +0800

    feat(code-language): add gnu c/c++ standard support

--- src/code-language/compile-and-run-options/c.ts
+++ src/code-language/compile-and-run-options/c.ts
@@ -4,7 +4,7 @@ export default class CompileAndRunOptionsC {
   @IsIn(["gcc", "clang"])
   compiler: string;
 
-  @IsIn(["c89", "c99", "c11", "c17"])
+  @IsIn(["c89", "c99", "c11", "c17", "gnu89", "gnu99", "gnu11", "gnu17"])
   std: string;
 
   @IsIn(["0", "1", "2", "3", "fast"])
--- src/code-language/compile-and-run-options/cpp.ts
+++ src/code-language/compile-and-run-options/cpp.ts
@@ -4,7 +4,7 @@ export default class CompileAndRunOptionsCpp {
   @IsIn(["g++", "clang++"])
   compiler: string;
 
-  @IsIn(["c++03", "c++11", "c++14", "c++17", "c++20"])
+  @IsIn(["c++03", "c++11", "c++14", "c++17", "c++20", "gnu++03", "gnu++11", "gnu++14", "gnu++17", "gnu++20"])
   std: string;
 
   @IsIn(["0", "1", "2", "3", "fast"])
