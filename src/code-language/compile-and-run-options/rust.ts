960cf3653fb5c9d620dbc00fb8279f5e3761ca4f
Menci <huanghaorui301@gmail.com>
2021-11-24 01:17:09 +0800

    feat(code-language): add Rust 2021 support

--- src/code-language/compile-and-run-options/rust.ts
+++ src/code-language/compile-and-run-options/rust.ts
@@ -1,7 +1,7 @@
 import { IsIn } from "class-validator";
 
 export default class CompileAndRunOptionsRust {
-  @IsIn(["2015", "2018"])
+  @IsIn(["2015", "2018", "2021"])
   version: string;
 
   @IsIn(["0", "1", "2", "3"])
